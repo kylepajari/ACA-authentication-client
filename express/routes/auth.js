c9abd97852e30b2e94c47015d175701c97c8a36f
Kyle Pajari <kylepajari@yahoo.com>
2019-04-24 15:23:25 -0500

    added private route

--- express/routes/auth.js
+++ express/routes/auth.js
@@ -1,16 +1,22 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const router = express.Router()
-const AuthController = require('../controllers/auth')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+const AuthController = require("../controllers/auth");
 
-router.post('/signup', (req, res) => {
+router.post("/signup", (req, res) => {
   AuthController.SignUp(req.body)
-    .then(() => res.send('User created successfully'))
-    .catch((err) => res.send(err.message))
-})
+    .then(() => res.send("User created successfully"))
+    .catch(err => res.send(err.message));
+});
 
-router.post('/login', (req, res) => {
-  // contents of login route
-})
+router.post("/login", (req, res) => {
+  AuthController.Login(req.body).then(result => {
+    if (!result) {
+      return res.status(404).send("user could not be found");
+    }
+    var token = jwt.sign({ ...result }, "secret");
+    return res.send(token);
+  });
+});
 
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const AuthRoutes = require('./express/routes/auth')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const AuthRoutes = require("./express/routes/auth");
 
-mongoose.connect(/* YOUR MONGO CONNECTION STRING */)
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0-1gjvo.mongodb.net/test?retryWrites=true"
+);
 
-const port = process.env.PORT || 4001
-const app = express()
+const port = process.env.PORT || 4001;
+const app = express();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'))
+  app.use(express.static("client/build"));
 }
 
-app.use(bodyParser.json())
-app.use('/auth', AuthRoutes)
+app.use(bodyParser.json());
+app.use("/auth", AuthRoutes);
 
-app.get('/', (req, res) => res.send('Default route!'))
+app.get("/", (req, res) => res.send("Default route!"));
 
 app.listen(port, () => {
-  console.log(`Express app running on localhost:${port}`)
-})
+  console.log(`Express app running on localhost:${port}`);
+});
